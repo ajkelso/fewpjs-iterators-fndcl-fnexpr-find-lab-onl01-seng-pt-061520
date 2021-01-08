d50130f2ff37eff96405ab93d846c1b50b9f273b
ajkelso <ajkelso@gmail.com>
2021-01-08 13:46:10 -0500

    commit tree 416e944b0836c320295566f5d9556384dc8c6c16

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let result = record.find(y => y.result === "W")
+  if (result !== undefined) {
+    return result.year
+  }
+}
